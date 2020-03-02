6d630f63d571ba8c2a3d8dd94199a7188f4362ce
KhaledAlabssi <khaled.absi5@gmail.com>
2020-03-02 07:35:16 +0100

    add authentication and test dummy users

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login
 
 # Create your views here.
 def signupuser(request):
@@ -8,7 +10,15 @@ def signupuser(request):
 		return render(request, 'todo/signupuser.html' , {'form': UserCreationForm()})
 	else:
 		if request.POST['password1'] == request.POST['password2']:
-			user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
-			user.save()
+			try:
+				user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+				user.save()
+				login(request, user)
+				return redirect('current')
+			except IntegrityError:
+				return render(request, 'todo/signupuser.html' , {'form': UserCreationForm(), 'error': 'Username has already been token, Please choose new username'})
 		else:
-			print('password1 not equal password2')
+			return render(request, 'todo/signupuser.html' , {'form': UserCreationForm(), 'error': 'Password did not match!'})
+
+def current(request):
+	return render(request, 'todo/current.html')
